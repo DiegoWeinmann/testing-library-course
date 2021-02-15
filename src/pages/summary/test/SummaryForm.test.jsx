c1203199d505f7c3805eca9e9e2ea2eb2875b25b
Diego Weinmann <diego.weinmann@hotmail.com>
2021-02-15 14:26:31 -0300

    feat: add popover to summary form and test

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,9 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import {
+	render,
+	screen,
+	waitForElementToBeRemoved
+} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import SummaryForm from '../SummaryForm'
 
 test('Initial conditions', () => {
@@ -26,9 +31,31 @@ test('Checkbox disables button on first click and enables on second click', () =
 		name: /confirm order/i
 	})
 
-	fireEvent.click(checkbox)
+	userEvent.click(checkbox)
 	expect(confirmButton).toBeEnabled()
 
-	fireEvent.click(checkbox)
+	userEvent.click(checkbox)
 	expect(confirmButton).toBeDisabled()
+})
+
+test('popover responds to hover', async () => {
+	render(<SummaryForm />)
+
+	// popover starts out hidden
+	const nullPopover = screen.queryByText(
+		/no ice cream will actually be delivered/i
+	)
+	expect(nullPopover).not.toBeInTheDocument()
+
+	// popover appears upon mouseover of checkbox label
+	const termsAndConditions = screen.getByText(/terms and conditions/i)
+	userEvent.hover(termsAndConditions)
+
+	const popover = screen.getByText(/no ice cream will actually be delivered/i)
+	expect(popover).toBeInTheDocument()
+
+	// popover disappears when we mouse out
+	await waitForElementToBeRemoved(() =>
+		screen.queryByText(/no ice cream will actually be delivered/i)
+	)
 })
